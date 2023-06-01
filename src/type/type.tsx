8960a6ef9053966dd19fa83a400568567e1bcc3d
gyucheolshin <nsfsgc2295@gmail.com>
2023-06-01 23:29:41 +0900

    feat : 데이터 타입 정의

---
+++ src/type/type.tsx
@@ -0,0 +1,13 @@
+export interface loginType {
+  email: string;
+  pwd: string;
+}
+
+export interface registType {
+  email: string;
+  pwd: string;
+  nickname: string;
+  userName: string;
+  authority: string;
+  userInfo: string;
+}
