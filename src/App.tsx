82aae453320a367a36907cafd54aaa6ea689ba5f
gyucheolshin <nsfsgc2295@gmail.com>
2023-06-01 23:38:21 +0900

    feat : 페이지 이동경로 정의

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Router from "./service/Router";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Router />
+    </div>
+  );
 }
 
 export default App;
