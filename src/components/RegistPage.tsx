7d1bafe3cdb354a5b4d4cdcfd15c613b46bc3a5a
gyucheolshin <nsfsgc2295@gmail.com>
2023-06-01 23:45:24 +0900

    feat : 회원가입 기능 구현

--- src/components/RegistPage.tsx
+++ src/components/RegistPage.tsx
@@ -1,6 +1,43 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { registType } from "../type/type";
+import { apiUrl } from "../service/authService";
+import axios from "axios";
 export default function RegistPage() {
+  const [registData, setRegistData] = useState<registType>({
+    email: "",
+    pwd: "",
+    nickname: "",
+    userName: "",
+    authority: "ROLE_ADMIN",
+    userInfo: "테스트 유저입니다.",
+  });
+
+  const data: registType = {
+    email: registData.email,
+    pwd: registData.pwd,
+    nickname: registData.nickname,
+    userName: registData.userName,
+    authority: registData.authority,
+    userInfo: registData.userInfo,
+  };
+
+  const registHandleSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    if (registData.pwd.length > 0 && registData.pwd.length < 12)
+      await axios
+        .post(`${apiUrl}`, data)
+        .then((res) => {
+          console.log("회원가입 성공");
+          console.log(res);
+        })
+
+        .catch((error) => {
+          console.log("회원가입 실패");
+          console.log(error);
+        });
+    else alert("비밀번호를 입력해주세요");
+  };
+
   return (
     <div className="w-full h-screen bg-gray-200 flex justify-center ">
       <div className="w-1/3 h-full flex-col">
@@ -16,6 +53,10 @@ export default function RegistPage() {
               className="w-full h-full border-black-200 rounded-l-xl text-2xl bg-white pl-4"
               placeholder="&#128100; 아이디 (예:battleq@naver.com)"
               autoComplete="off"
+              value={registData.email}
+              onChange={(e) =>
+                setRegistData({ ...registData, email: e.target.value })
+              }
             />
             <button className="w-1/6 h-full bg-white flex justify-center items-center rounded-r-xl">
               <img
@@ -32,6 +73,10 @@ export default function RegistPage() {
               className="w-full h-full border-black-200 rounded-l-xl text-2xl bg-white pl-4"
               placeholder="&#128274; 비밀번호"
               autoComplete="off"
+              value={registData.pwd}
+              onChange={(e) =>
+                setRegistData({ ...registData, pwd: e.target.value })
+              }
             />
           </div>
           <div className="w-5/6 h-10percent mb-5 flex">
@@ -40,6 +85,10 @@ export default function RegistPage() {
               className="w-full h-full border-black-200 rounded-l-xl text-2xl bg-white pl-4"
               placeholder="&#128587; 이름"
               autoComplete="off"
+              value={registData.userName}
+              onChange={(e) =>
+                setRegistData({ ...registData, userName: e.target.value })
+              }
             />
           </div>
           <div className="w-5/6 h-10percent mb-12 flex">
@@ -48,6 +97,10 @@ export default function RegistPage() {
               className="w-full h-full border-black-200 rounded-l-xl text-2xl bg-white pl-4 "
               placeholder="&#128055; 닉네임"
               autoComplete="off"
+              value={registData.nickname}
+              onChange={(e) =>
+                setRegistData({ ...registData, nickname: e.target.value })
+              }
             />
             <button className="w-1/6 h-full bg-white flex justify-center items-center rounded-r-xl">
               <img
@@ -59,7 +112,10 @@ export default function RegistPage() {
           </div>
 
           <div className="w-full h-1/6 flex justify-center items-center">
-            <button className="w-3/4 h-1/2 text-4xl font-semibold rounded-lg bg-violet-400 text-white">
+            <button
+              className="w-3/4 h-1/2 text-4xl font-semibold rounded-lg bg-violet-400 text-white"
+              onClick={registHandleSubmit}
+            >
               회원가입
             </button>
           </div>
