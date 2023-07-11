0839f750d704873ef1e6c73ec7483e0c7b963816
gyucheolshin <nsfsgc2295@gmail.com>
2023-07-11 18:17:47 +0900

    feat : 유저정보 조회 및 저장

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import Router from "./service/Router";
-import axios from "axios";
-import { email, headers } from "./service/authService";
+import { fetchData } from "./redux/reducer/userDataSlice";
+import { useAppDispatch } from "./redux/hooks/hooks";
 
 function App() {
+  const dispatch = useAppDispatch();
+
   useEffect(() => {
-    (async () => {
-      await axios
-        .get(`member/detail/${email}`, {
-          headers,
-        })
-        .then(
-          (res) => {},
-          (err) => {}
-        );
-    })();
-  }, [email]);
+    dispatch(fetchData());
+  }, []);
 
   return (
     <div className="w-screen h-screen">
--- src/redux/reducer/userDataSlice.tsx
+++ src/redux/reducer/userDataSlice.tsx
@@ -1,44 +1,27 @@
 import axios from "axios";
-import { RootState } from "../store/store";
-import {
-  AnyAction,
-  PayloadAction,
-  ThunkAction,
-  createSlice,
-} from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-import { userData, userDataArray } from "../../type/type";
+import { userDataArray } from "../../type/type";
 import { email, headers } from "../../service/authService";
-
 const initialState: userDataArray = {
   userdata: [],
 };
 
-export const fetchInfos = (): ThunkAction<
-  void,
-  RootState,
-  unknown,
-  AnyAction
-> => {
-  return async (dispatch) => {
-    const response: userData[] = await axios.get(`member/detail/${email}`, {
-      headers,
-    });
-    dispatch(infoActions.setUserData(response));
-    console.log(response);
-  };
-};
+export const fetchData = createAsyncThunk("data/fetchData", async () => {
+  const response = await axios.get(`member/detail/${email}`, { headers });
+  const userData = response.data;
+  return userData;
+});
 
 export const userDataSlice = createSlice({
   name: "userdata",
   initialState,
-  reducers: {
-    setUserData: (state, action: PayloadAction<userData[]>) => {
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchData.fulfilled, (state, action) => {
       state.userdata = action.payload;
-    },
+    });
   },
 });
-
-export const infoActions = userDataSlice.actions;
 
 export default userDataSlice.reducer;
